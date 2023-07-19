09c80963911e055259f7a77b7f70a3e705bce929
Pawel Konior <pawel.konior@gmail.com>
2023-07-19 15:06:07 +0200

    user inputs validation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,10 +4,27 @@ const inputRefs = [...document.querySelectorAll('input[id^="digit"]')];
 
 const isAllNotEmpty = (nodes) => nodes.every((node) => node.value.trim() !== '');
 const isIntegers = (nodes) => nodes.every((node) => Number.isInteger(Number(node.value)));
+const convertToIntegers = (nodes) => nodes.map((node) => parseInt(node.value));
+const isInRange = (digits) => digits.every((digit) => digit >= 1 && digit <= 49);
+const isNotRedundant = (digits) => new Set(digits).size === digits.length;
+
 
 btnPlayRef.addEventListener('click', function (event) {
     if (isAllNotEmpty(inputRefs)) {
-        console.log('ok')
+        if (isIntegers(inputRefs)) {
+            const userDigits = convertToIntegers(inputRefs);
+            if (isInRange(userDigits)){
+                if(isNotRedundant(userDigits)){
+
+                } else {
+                    console.log('Liczby ci się powtarzają ziomeczku')
+                }
+            }else {
+                console.log('są liczby poza zakresem 1-49')
+            }
+        } else {
+            console.log('liczby nie są liczbami')
+        }
     } else {
         console.log('coś jest puste')
     }
