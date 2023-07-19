5a4ecc291a442af344820e027a00d2dc20eb36a1
Pawel Konior <pawel.konior@gmail.com>
2023-07-19 13:10:12 +0200

    basic inputs validation after user click

---
+++ src/scripts/app.js
@@ -0,0 +1,14 @@
+const btnPlayRef = document.querySelector('#playBtn');
+const inputRefs = [...document.querySelectorAll('input[id^="digit"]')];
+
+
+const isAllNotEmpty = (nodes) => nodes.every((node) => node.value.trim() !== '');
+const isIntegers = (nodes) => nodes.every((node) => Number.isInteger(Number(node.value)));
+
+btnPlayRef.addEventListener('click', function (event) {
+    if (isAllNotEmpty(inputRefs)) {
+        console.log('ok')
+    } else {
+        console.log('coś jest puste')
+    }
+})
